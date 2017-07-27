9b7d53d5fdf86dc49bd69d2338e3fc770459ce9b
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-07-27 21:07:39 +0300

    Write numbers and point

--- js/calculator.js
+++ js/calculator.js
@@ -3,11 +3,36 @@ $(document).ready(function () {
     $('.calculator').append("<input disabled value='0'>");
 
     $.each(data, function (index, data) {
-        $(".calculator").append($("<button>" + data.value + "</button>").
-            attr("class", data.class).
-            attr("type", data.type).
-            attr("value", data.value).
-            attr("disable", false)
+        $(".calculator").append($("<button>" + data.value + "</button>").attr("class", data.class).attr("type", data.type).attr("value", data.value).attr("disable", false)
         );
     });
+});
+
+
+var a = '0';
+
+function handleClick(e) {
+    var $b = $(e.currentTarget);
+
+    if ($b.attr('type') == 'number') {
+        if ($b.val() === '.') {
+            if (a.indexOf('.') === -1) {
+                a += $b.val();
+                $('input').val(a);
+            }
+        } else {
+            a += $b.val();
+            $('input').val(a);
+        }
+    }
+
+    else if ($b.attr('type') == 'action') {
+
+
+    }
+
+
+}
+$(document).ready(function () {
+    $("button").click(handleClick);
 });
